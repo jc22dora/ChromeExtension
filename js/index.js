94c6409de6232e7a954c26ea9c6a300cdfb6be1d
johnd <jc22dora@siena.edu>
2022-02-11 14:02:23 -0500

    Working chat room

--- js/index.js
+++ js/index.js
@@ -1,27 +1,47 @@
 const WebSocket = require('ws');
+const uuid = require('uuid');
+
 
 const wss = new WebSocket.Server({ port: 8082 });
 
 wss.on("connection", ws => {
-    console.log("new client connected");
+    ws.id = uuid.v4();
+    console.log(ws.id + " client connected");
 
     ws.on("message", data => {
-        data.toJSON();
-        console.log(JSON.stringify(data));
         if (data.type === 'set-login') {
             console.log('server received login data')
         }
+        try {
+            var msg = JSON.parse(data);
+            console.log(msg.type);
+            console.log(msg.username);
+            console.log(msg.text);
+            if (msg.type === 'message') {
+                wss.broadcast(data);
+                console.log('server broadcasted message to clients');
+            }
+            else {
+                console.log(msg.type);
+            }
+        }
+        catch (e){
+            console.log(e);
+        }
+        
         console.log(`Client sent....${data}`);
         //ws.send('server received message ');
-        wss.broadcast(data);
-        console.log('server broadcasted message to clients');
+        
+        
     })
     ws.on("close", () => {
         console.log("client disconnected");
     });
 })
 wss.broadcast = function (data) {
-    wss.clients.forEach(client => client.send(data));
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(data));
+    });
 };
 function bufferFromBufferString(bufferStr) {
     return Buffer.from(
